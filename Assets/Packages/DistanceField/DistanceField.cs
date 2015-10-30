fdc8e8bc2df7e80e27f8a881a34b9866c98d74df
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-10-30 17:09:58 +0900

    no message

--- Assets/Packages/DistanceField/DistanceField.cs
+++ Assets/Packages/DistanceField/DistanceField.cs
@@ -1,24 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 namespace DistanceFieldSystem {
 
 	public class DistanceField : MonoBehaviour {
 		public enum DebugModeEnum { Normal = 0, Debug }
 		public enum HeightModeEnum { Distance = 0, Metaball }
-		[System.Flags]
 		public enum ViewModeEnum { Normal = 0, Height, LIC, Flow }
+		public enum NormalTexModeEnum { Tex2D = 0, RenderTex }
+
+		public Texture2DEvent OnReady;
+
 		public int lod = 1;
+		public float interval = 1f;
 		public int pointCapacity = 64;
 		public DebugModeEnum debugMode;
 		public HeightModeEnum heightMode;
 		public ViewModeEnum viewMode;
+		public NormalTexModeEnum normalTexMode;
 		public Camera targetCamera;
 		public Material distMat;
 		public Transform[] points;
 
 		RenderTexture _distTex;
 		RenderTexture _normTex;
+		Texture2D _normTex2D;
 		PointService _points;
 		Renderer _renderer;
 		MaterialPropertyBlock _viewProps;
@@ -35,17 +42,31 @@ namespace DistanceFieldSystem {
 			_viewProps = new MaterialPropertyBlock();
 			_renderer = GetComponent<Renderer>();
 			_renderer.SetPropertyBlock(_viewProps);
-		}
-		void Update() {
 			CheckInit();
-			_points.Update(points, targetCamera);
-			_points.SetData(distMat);
-			SetData(distMat);
-			Graphics.Blit(null, _distTex, distMat, 0);
-			Graphics.Blit(_distTex, _normTex, distMat, 1);
-			_renderer.SetPropertyBlock(_viewProps);
+			StartCoroutine(Repeater());
+		}
+		IEnumerator Repeater() {
+			while (true) {
+				yield return new WaitForSeconds(interval);
 
-			SetViewMode();
+				CheckInit();
+				_points.Update(points, targetCamera);
+				_points.SetData(distMat);
+				SetData(distMat);
+				Graphics.Blit(null, _distTex, distMat, 0);
+				Graphics.Blit(_distTex, _normTex, distMat, 1);
+				_renderer.SetPropertyBlock(_viewProps);
+
+				SetViewMode();
+
+				yield return new WaitForEndOfFrame();
+				var prevTex = RenderTexture.active;
+				RenderTexture.active = _normTex;
+				_normTex2D.ReadPixels(new Rect(0, 0, _normTex2D.width, _normTex2D.height), 0, 0);
+				_normTex2D.Apply();
+				RenderTexture.active = prevTex;
+				OnReady.Invoke(_normTex2D);
+			}
 		}
 		void OnDestroy() {
 			Release();
@@ -72,6 +93,8 @@ namespace DistanceFieldSystem {
 				_normTex.wrapMode = TextureWrapMode.Clamp;
 				_normTex.antiAliasing = _distTex.antiAliasing;
 				_viewProps.SetTexture(ShaderConsts.PROP_NORM_TEXTURE, _normTex);
+
+				_normTex2D = new Texture2D(width, height, TextureFormat.ARGB32, false, true);
 			}
 			if (_points == null) {
 				_points = new PointService(pointCapacity);
@@ -119,6 +142,15 @@ namespace DistanceFieldSystem {
 				_viewProps.SetVector(ShaderConsts.PROP_VIEW_FILTER, new Vector4(1, 0, 0, 0));
 				break;
 			}
+
+			switch (normalTexMode) {
+			case NormalTexModeEnum.RenderTex:
+				_viewProps.SetTexture(ShaderConsts.PROP_NORM_TEXTURE, _normTex);
+				break;
+			default:
+				_viewProps.SetTexture(ShaderConsts.PROP_NORM_TEXTURE, _normTex2D);
+				break;
+			}
 		}
 		void Release() {
 			if (_distTex != null) {
@@ -129,10 +161,17 @@ namespace DistanceFieldSystem {
 				Destroy(_normTex);
 				_normTex = null;
 			}
+			if (_normTex2D != null) {
+				Destroy(_normTex2D);
+				_normTex2D = null;
+			}
 			if (_points != null) {
 				_points.Dispose();
 				_points = null;
 			}
 		}
 	}
+
+	[System.Serializable]
+	public class Texture2DEvent : UnityEvent<Texture2D> {}
 }
